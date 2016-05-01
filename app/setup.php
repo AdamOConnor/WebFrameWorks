9735b4520fc091c9ce9e6218e9162e21e7fff4fa
Adam O'Connor <adam-o-connor@hotmail.com>
2016-05-01 23:00:25 +0100

    configdb sorted

--- app/setup.php
+++ app/setup.php
@@ -1,6 +1,7 @@
 <?php
 //----- autoload any classes we are using ------
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/config_db.php';
 
 //----- Twig setup --------------
 $templatesPath = __DIR__ . '/../templates';
